af0522c8ded071eae30922331483332f56b9c57a
iantsai <iantsai86@gmail.com>
2015-05-09 20:27:09 -0700

    first commit

---
+++ AdventureGame/Player.m
@@ -0,0 +1,13 @@
+//
+//  Player.m
+//  AdventureGame
+//
+//  Created by Ian Tsai on 2015-05-09.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
